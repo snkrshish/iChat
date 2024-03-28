30fd636ab84cba45733e68e4225f2818bb84cdf3
Nikita Shilyaev <92994263+snkrshish@users.noreply.github.com>
2024-03-28 15:22:48 +0800

    Initial Commit

---
+++ iChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iChat
+//
+//  Created by Никита Шиляев on 28.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
